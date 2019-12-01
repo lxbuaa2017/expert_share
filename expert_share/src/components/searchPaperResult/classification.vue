d4a220d40b5b92434356c0aaf14501a38625af2d
lx <1753312844@qq.com>
2019-12-01 11:54:27 +0800

    基本完善了searchPaperResult

--- expert_share/src/components/searchPaperResult/classification.vue
+++ expert_share/src/components/searchPaperResult/classification.vue
@@ -1,22 +1,23 @@
 <template>
   <div>
   <Menu active-name="1-2" :open-names="['1']">
-    <Submenu name="1" v-for="i in year" :key="i">
+    <Submenu name="1">
       <template slot="title">
         <Icon type="ios-filing" />
         年份
       </template>
-      <MenuItem name=i>i</MenuItem>
+      <div v-for="i in this.year" :key="i">
+        <MenuItem name=i>{{i}}</MenuItem>
+      </div>
     </Submenu>
     <Submenu name="3">
       <template slot="title">
         <Icon type="ios-cog" />
         作者
       </template>
-      <MenuItem name="3-1">Option 9</MenuItem>
-      <MenuItem name="3-2">Option 10</MenuItem>
-      <MenuItem name="3-3">Option 11</MenuItem>
-      <MenuItem name="3-4">Option 12</MenuItem>
+      <div v-for="i in this.author" :key="i">
+      <MenuItem name=i>{{i}}</MenuItem>
+      </div>
     </Submenu>
   </Menu>
   </div>
