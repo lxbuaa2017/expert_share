cf0e13ef8f1ad2070eaa49ad2862d3a6f4e9b74e
lx <1753312844@qq.com>
2019-12-12 23:41:38 +0800

    bug fix

--- expert_share/src/components/views/searchPaperResult.vue
+++ expert_share/src/components/views/searchPaperResult.vue
@@ -94,16 +94,17 @@
             // })
             // get_papers
             window.localStorage.removeItem("input_keyword")
+            this.page_change(1)
         },
         methods:{
           page_change($event){
             this.page_id=$event
             this.paper_result_flag=false
             this.$nextTick(()=>{
-                this.input_keyword='基于'
+                // this.input_keyword='基于'
                 let self=this
                 ///api/get_paper_by_page?page='+self.page_id+'&keyword='+self.input_keyword
-                this.$axios.get('/api/get_papers').then((res)=>{
+                this.$axios.get('api/get_paper_by_page?page='+self.page_id+'&keyword='+self.input_keyword).then((res)=>{
                     console.log(res.data)
                     this.papers=res.data
                 })
@@ -128,7 +129,7 @@
               author:['张伟','向华强'],
               input_keyword:'',
               papers:[
-                  { "_id" : { "$oid" : "5de33f210031bb949dbd6831" }, "c_abstract" : "针对微小型导航系统对导航计算机处理能力、体积、功耗及适应性等方面的要求,以捷联导航系统为例,使用SOPC方法在FPGA上设计嵌入式高性能导航计算机系统,采用嵌入式实时操作系统(RTOS)作为软件开发平台,重点研究了系统硬件平台的设计以及系统应用程序的开发.测试结果表明该系统在姿态、位置、速度等参数误差满足要求,具有很高的实用价值.", "c_author" : "李良仁,汪临伟,彭雪峰", "c_keywords" : "SOPC,FPGA,导航计算机,RTOS", "c_periodical" : "制造业自动化", "c_title" : "基于SOPC的嵌入式导航计算机设计", "e_periodical" : "MANUFACTURING AUTOMATION", "e_title" : "Design of the embedded navigation computer based on SOPC", "fund" : "", "indexID" : "2010, 32(1)", "time" : "2010年3月30日", "units" : "九江职业技术学院电气工程系,九江,332007", "url" : "http://d.old.wanfangdata.com.cn/Periodical/zzyzdh201001047" }
+                  { "id" : "5de33f210031bb949dbd6831", "c_abstract" : "针对微小型导航系统对导航计算机处理能力、体积、功耗及适应性等方面的要求,以捷联导航系统为例,使用SOPC方法在FPGA上设计嵌入式高性能导航计算机系统,采用嵌入式实时操作系统(RTOS)作为软件开发平台,重点研究了系统硬件平台的设计以及系统应用程序的开发.测试结果表明该系统在姿态、位置、速度等参数误差满足要求,具有很高的实用价值.", "c_author" : "李良仁,汪临伟,彭雪峰", "c_keywords" : "SOPC,FPGA,导航计算机,RTOS", "c_periodical" : "制造业自动化", "c_title" : "基于SOPC的嵌入式导航计算机设计", "e_periodical" : "MANUFACTURING AUTOMATION", "e_title" : "Design of the embedded navigation computer based on SOPC", "fund" : "", "indexid" : "2010, 32(1)", "time" : "2010年3月30日", "units" : "九江职业技术学院电气工程系,九江,332007", "url" : "http://d.old.wanfangdata.com.cn/Periodical/zzyzdh201001047" }
 
               ]
           }
