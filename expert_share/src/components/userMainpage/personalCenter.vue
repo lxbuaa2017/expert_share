8f9a232063f0b4e526d68a496ca69736412fa897
chuximing <chuximing@qq.com>
2019-12-12 23:18:58 +0800

    finished userMainpage

---
+++ expert_share/src/components/userMainpage/personalCenter.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "personalCenter"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ expert_share/src/components/views/userMainpage.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "userMainpage"
+    }
+</script>
+
+<style scoped>
+
+</style>
