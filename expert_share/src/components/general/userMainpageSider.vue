ce8aadd9498d4da1f9712a76376b1ed93112e787
chuximing <chuximing@qq.com>
2019-12-14 22:23:43 +0800

    working

--- expert_share/src/components/general/userMainpageSider.vue
+++ expert_share/src/components/general/userMainpageSider.vue
@@ -1,56 +1,54 @@
 <template>
   <Content>
-    <Sider hide-trigger :style="{background: '#fff'}">
-      <Menu theme="light" width="auto" :open-names="['1']">
-        <Submenu name="1">
-          <template slot="title">
+    <Menu theme="light" width="auto" :open-names="['1']">
+      <Submenu name="1">
+        <template slot="title">
+          个人中心
+        </template>
+        <MenuItem name="1-1">
+          <router-link :to="{path: '/userMainpage'}">
             个人中心
-          </template>
-          <MenuItem name="1-1">
-            <router-link :to="{path: '/userMainpage'}">
-              个人中心
-            </router-link>
-          </MenuItem>
-          <MenuItem name="1-2">
-            <router-link :to="{path: '/userFavorites'}">
-              我的收藏
-            </router-link>
-          </MenuItem>
-          <MenuItem name="1-3">
-            <div>
-              我的关注
-            </div>
-          </MenuItem>
-        </Submenu>
-        <Submenu name="2">
-          <template slot="title">
-            管理平台
-          </template>
-          <MenuItem name="2-1" v-if="userType ==='user'">
-            <div>
-              申请成为专家
-            </div>
-          </MenuItem>
-          <MenuItem name="2-2" v-if="userType ==='expert'">
-            我的论文
-          </MenuItem>
-          <MenuItem name="2-3" v-if="userType ==='expert'">
-            我的项目
-          </MenuItem>
-          <MenuItem name="2-4" v-if="userType ==='expert'">
-            我的专利
-          </MenuItem>
-        </Submenu>
-        <Submenu name="3">
-          <template slot="title">
-            其他
-          </template>
-          <MenuItem name="3-1">
-            联系我们
-          </MenuItem>
-        </Submenu>
-      </Menu>
-    </Sider>
+          </router-link>
+        </MenuItem>
+        <MenuItem name="1-2">
+          <router-link :to="{path: '/userFavorites'}">
+            我的收藏
+          </router-link>
+        </MenuItem>
+        <MenuItem name="1-3">
+          <div>
+            我的关注
+          </div>
+        </MenuItem>
+      </Submenu>
+      <Submenu name="2">
+        <template slot="title">
+          管理平台
+        </template>
+        <MenuItem name="2-1" v-if="userType ==='user'">
+          <div>
+            申请成为专家
+          </div>
+        </MenuItem>
+        <MenuItem name="2-2" v-if="userType ==='expert'">
+          我的论文
+        </MenuItem>
+        <MenuItem name="2-3" v-if="userType ==='expert'">
+          我的项目
+        </MenuItem>
+        <MenuItem name="2-4" v-if="userType ==='expert'">
+          我的专利
+        </MenuItem>
+      </Submenu>
+      <Submenu name="3">
+        <template slot="title">
+          其他
+        </template>
+        <MenuItem name="3-1">
+          联系我们
+        </MenuItem>
+      </Submenu>
+    </Menu>
   </Content>
 </template>
 
