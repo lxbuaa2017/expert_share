62681acb2e84731e8462ee91c51c13ed16189401
yjj9240 <49605926+yjj9240@users.noreply.github.com>
2019-12-14 22:13:26 +0800

    主页 登录 注册 页面

--- expert_share/src/router/index.js
+++ expert_share/src/router/index.js
@@ -40,6 +40,11 @@ export default new Router({
       path: '/test',
       name: 'test',
       component: test
+    },
+    {
+      path: '/highsearch',
+      name: 'highsearch',
+      component: test
     }
   ]
 })
