bdea4f1b9cfd8810b2ca9d1f9254cfc2ba53abb8
lx <1753312844@qq.com>
2019-12-20 17:48:41 +0800

    fix

--- expert_share/src/components/views/searchPaperResult.vue
+++ expert_share/src/components/views/searchPaperResult.vue
@@ -60,11 +60,38 @@
         </Header>
         <Layout :style="{padding: '0 50px'}">
           <Sider hide-trigger :style="{background: '#fff'}">
-            <classification :papers="papers" :keyword="input_keyword"
-                            :author="author" :year="year" :paper_result_flag="paper_result_flag"
-                            :by_author="by_author" :by_year="by_year" :which_author="which_author" :which_year="which_year"
-                            :paper_cnt="paper_cnt"
-            ></classification>
+<!--            <classification :papers="papers" :keyword="input_keyword"-->
+<!--                            :author="author" :year="year" :paper_result_flag="paper_result_flag"-->
+<!--                            :by_author="by_author" :by_year="by_year" :which_author="which_author" :which_year="which_year"-->
+<!--                            :paper_cnt="paper_cnt"-->
+<!--            ></classification>-->
+            <div>
+              <Menu active-name="1-2" :open-names="['1']">
+                <Submenu name="1">
+                  <template slot="title">
+                    <Icon type="ios-filing" />
+                    年份
+                  </template>
+                  <div v-for="i in this.year" :key="i">
+                    <MenuItem name=i>
+                      <span @click="show_by_year(i)">{{i}}</span>
+                    </MenuItem>
+                  </div>
+                </Submenu>
+                <Submenu name="3">
+                  <template slot="title">
+                    <Icon type="ios-cog" />
+                    作者
+                  </template>
+                  <div v-for="i in this.author" :key="i">
+                    <MenuItem name=i>
+                      <span @click="show_by_author(i)">{{i}}</span>
+                    </MenuItem>
+                  </div>
+                </Submenu>
+              </Menu>
+            </div>
+
           </Sider>
           <Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">
             <div style="padding-left: 22px;padding-top: 0px">
@@ -108,6 +135,31 @@
             })
         },
         methods:{
+            show_by_year(i){
+                this.which_year=i
+                //http://localhost:8000/get_paper_total_by_year?keyword=数据挖掘&year=2019
+                let self = this
+                this.$axios.get('api/get_paper_total_by_year/?keyword='+self.keyword+'&year='+i).then((res)=>{
+                    self.paper_cnt = res.data
+                })
+                this.by_year = true
+                this.by_author = false
+                this.page_change(1)
+                // alert('按年展示 '+i)
+            },
+            show_by_author(i){
+
+                this.which_author=i
+                //http://localhost:8000/get_paper_total_by_year?keyword=数据挖掘&year=2019
+                let self = this
+                this.$axios.get('api/get_paper_total_by_author/?keyword='+self.keyword+'&author='+i).then((res)=>{
+                    self.paper_cnt = res.data
+                    alert(this.which_author)
+                })
+                self.by_year = false
+                self.by_author = true
+               this.page_change(1)
+            },
           page_change($event){
             this.page_id=$event
             this.paper_result_flag=false
